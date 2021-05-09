b07c00f90e83629d1cc41b4a98f7a79a6404e843
addubinski <addubinski@gmail.com>
2021-05-09 12:07:52 +0900

    add common objects and first animations

--- yankee-tech/top_five_data_structures.py
+++ yankee-tech/top_five_data_structures.py
@@ -1,6 +1,28 @@
 from manim import *
+from common import generic_object_symbol
+
 
 class Lists(Scene):
+
+    def construct(self):
+
+        object_symbol = Text(generic_object_symbol)
+        blocks = [
+            Square(side_length=1.25).move_to(LEFT * 3),
+            Square(side_length=1.25),
+            Square(side_length=1.25),
+            Square(side_length=1.25),
+            Square(side_length=1.25)
+        ]
+
+        for idx, block in enumerate(blocks[1:]):
+            block.next_to(blocks[idx])
+
+        self.play(*[Create(block) for block in blocks])
+        self.wait(2)
+
+
+class LinkedLists(Scene):
 
     def construct(self):
 
@@ -13,5 +35,37 @@ class Lists(Scene):
             Square()
         ]
 
-        self.play(Create(blocks[1]))
+
+class BinaryTrees(Scene):
+
+    def construct(self):
+
+        blocks = [
+            Circle(),
+            Circle(),
+            Circle(),
+            Circle(),
+            Circle(),
+            Circle()
+        ]
+
+
+class Graphs(Scene):
+
+    def construct(self):
+
+        blocks = [
+            Circle(),
+            Circle(),
+            Circle(),
+            Circle(),
+            Circle(),
+            Circle()
+        ]
+
+
+class HashTables(Scene):
+
+    def construct(self):
+        pass
 
